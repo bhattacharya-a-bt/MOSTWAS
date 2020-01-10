ced86df9e8b51f625651704f6a7581e51ae82826
mikelove <michaelisaiahlove@gmail.com>
2020-01-10 13:10:33 -0500

    moving sample data

--- tests/testthat/test_trainExpression.R
+++ tests/testthat/test_trainExpression.R
@@ -1,13 +1,15 @@
 test_that("train expression works", {
 
+  dir = system.file('extdata', package='MOSTWAS')
+  
   a = trainExpression(geneInt = 'test',
-                      snps = data.table::fread('sample_data/snps.txt'),
-                      snpLocs = data.table::fread('sample_data/snpLocs.txt'),
-                      mediator = data.table::fread('sample_data/mediators.txt'),
-                      medLocs = read.table('sample_data/medLocs.txt',header=T),
+                      snps = data.table::fread(file.path(dir,'snps.txt')),
+                      snpLocs = data.table::fread(file.path(dir,'snpLocs.txt')),
+                      mediator = data.table::fread(file.path(dir,'mediators.txt')),
+                      medLocs = read.table(file.path(dir,'medLocs.txt',header=T)),
                       covariates = NULL,
-                      qtlFull = rbind(data.table::fread('sample_data/testCis.txt'),
-                                      data.table::fread('sample_data/testTra.txt')),
+                      qtlFull = rbind(data.table::fread(file.path(dir,'testCis.txt')),
+                                      data.table::fread(file.path(dir,'testTra.txt'))),
                       numMed = 5,
                       seed = 1,
                       k = 5,
@@ -16,7 +18,7 @@ test_that("train expression works", {
                       prune = F,
                       cores = 1)
 
-  truth = data.table::fread('sample_data/truth.txt')
+  truth = data.table::fread(file.path(dir,'truth.txt'))
 
 })
 
