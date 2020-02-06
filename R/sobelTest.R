b1c75446ddc402d48d9faf6e21955b3ebbfcc629
Arjun Bhattacharya <abhattac@live.unc.edu>
2020-02-06 09:34:37 -0500

    p value calc

--- R/sobelTest.R
+++ R/sobelTest.R
@@ -48,6 +48,6 @@ sobelTest <- function(snp,
   cov = sqrt(as.numeric(t(c(b,a)) %*% V %*% c(b,a)))
 
   return(list(test.stat = TME,
-              p.value = pnorm(abs(TME/cov))))
+              p.value = 2*pnorm(-abs(TME/cov))))
 
 }
