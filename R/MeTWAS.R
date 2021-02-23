b17cf2eb2316e5f98eccfc4ac92a76cf6d7071fa
Arjun Bhattacharya <abhattac@live.unc.edu>
2021-02-23 11:34:18 -0500

    metwas

--- R/MeTWAS.R
+++ R/MeTWAS.R
@@ -181,7 +181,7 @@ MeTWAS <- function(geneInt,
                           nfolds = k)
   }
   names(medTrainList) = medList
-  medTrainList[sapply(medTrainList,length) > 0]
+  medTraitList = medTrainList[sapply(medTrainList,length) > 0]
 
   print('FITTING MEDIATORS')
   fe.R2 = 0
